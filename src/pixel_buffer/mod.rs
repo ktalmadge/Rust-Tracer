afdaad7215f56fe662b257115b5daa94258819d6
Kevin Talmadge <kevin.i.talmadge@gmail.com>
2017-09-30 17:02:41 -0700

    Modified to use ImageBuffer instead of slice

--- src/pixel_buffer/mod.rs
+++ src/pixel_buffer/mod.rs
@@ -1,36 +1,45 @@
 extern crate image;
 
-pub struct PixelBuffer<'a> {
-    size_x: usize,
-    size_y: usize,
-    buffer: &'a mut [u8],
+use self::image::{ImageBuffer, Pixel, Rgba};
+use std::path::Path;
+use std::io;
+
+pub struct PixelBuffer {
+    width: usize,
+    height: usize,
+    image_buffer: ImageBuffer<Rgba<u8>, Vec<u8>>,
 }
 
-impl<'a> PixelBuffer<'a> {
-    pub fn new(x: usize, y: usize, initial_buffer: &'a mut [u8]) -> PixelBuffer {
-        PixelBuffer {
-            size_x: x,
-            size_y: y,
-            buffer: initial_buffer,
+impl PixelBuffer {
+    pub fn new(width: usize, height: usize) -> PixelBuffer {
+        let mut pb = PixelBuffer {
+            width,
+            height,
+            image_buffer: ImageBuffer::new(width as u32, height as u32),
+        };
+
+        pb.clear_image_buffer();
+
+        pb
+    }
+
+    pub fn set_pixel(&mut self, x: usize, y: usize, r: u8, g: u8, b: u8, a: u8) {
+        self.image_buffer.put_pixel(
+            x as u32,
+            y as u32,
+            Rgba::from_channels(r, g, b, a),
+        );
+    }
+
+    pub fn clear_image_buffer(&mut self) {
+        for x in 0..self.width {
+            for y in 0..self.height {
+                self.set_pixel(x, y, 0, 0, 0, 255);
+            }
         }
     }
 
-    pub fn set_pixel(&mut self, x: usize, y: usize, r: u8, g: u8, b: u8) {
-        // In english: Skip Y rows of SIZE_X, offset by X; 3 bytes per pixel
-        let pixel_offset: usize = y * self.size_x * 3 + x * 3;
-
-        self.buffer[pixel_offset] = r;
-        self.buffer[pixel_offset + 1] = g;
-        self.buffer[pixel_offset + 2] = b;
-    }
-
-    pub fn write_buffer(&mut self, filename: &str) {
-        image::save_buffer(
-            filename,
-            self.buffer,
-            self.size_x as u32,
-            self.size_y as u32,
-            image::RGB(8),
-        ).unwrap();
+    pub fn save_image(&mut self, filename: &str) -> io::Result<()> {
+        self.image_buffer.save(Path::new(filename))
     }
 }
